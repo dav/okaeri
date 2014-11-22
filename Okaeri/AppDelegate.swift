134b87ff7774aec714f6911ba14894d2248cf357
Dav Yaginuma <dav@akuaku.org>
2014-11-22 10:04:56 -0800

    Initial Commit

---
+++ Okaeri/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Okaeri
+//
+//  Created by Dav on 11/22/14.
+//  Copyright (c) 2014 Sekai No. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
